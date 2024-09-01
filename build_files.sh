32bcd85285191a95d6f792533129f38097c6dc58
Mori-kamiyama <morikawa.yuta7@gmail.com>
2024-09-01 23:35:18 +0900

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
-# Ensure pip is installed
-if ! command -v pip &> /dev/null
-then
-    echo "pip could not be found, installing pip..."
-    apt-get update && apt-get install -y python3-pip
-fi
 
-# Create and activate a virtual environment
-python -m venv venv
-source venv/bin/activate
-
-# Install dependencies
+# Vercelが提供するPython環境をアクティベート
 pip install -r requirements.txt
 
-# Run Django management commands or other build steps
-python manage.py collectstatic --noinput
-python manage.py migrate
-
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+# Django管理コマンドを実行
+python3.9 manage.py migrate  # マイグレーションを実行
+python3.9 manage.py collectstatic --noinput  # 静的ファイルを収集
