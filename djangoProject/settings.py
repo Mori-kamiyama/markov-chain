457faf3cc4a715ed3581696b87cdc366c47135f1
Mori-kamiyama <morikawa.yuta7@gmail.com>
2024-09-01 23:24:30 +0900

    first commit

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-r!6ys6-+fgen(#r*kdiz!4-kh0x4dtvy+_x*%=3tayz(6_s+z#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*',
+    '.vercel.app'
+]
 
 
 # Application definition
@@ -105,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
